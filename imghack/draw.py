659632275126e788a04e0bdb730354f5e51955e2
Hanly <2116893813@qq.com>
2023-07-27 10:41:40 +0800

    updata

--- imghack/draw.py
+++ imghack/draw.py
@@ -8,11 +8,10 @@ height = 256
 # 创建一个白色图像
 img_0 = Image.new('RGB', (width, height), color='white')
 
-for u in range(0,16):
-    for i in range(u*16,u*16+16):
-        for y in range(i,i+1):
-            for x in range(img_0.width):
-                img_0.putpixel((x, y), (255-(i-u*16)*16, 255-(i-u*16)*16, 255-(i-u*16)*16))
+for i in range(0,16):
+    for y in range(i*16,i*16+16):
+        for x in range(img_0.width):
+            img_0.putpixel((x, y), (255-i*16, 255-i*16, 255-i*16))
 
 # 保存图像
 img_0.save("img_0.bmp")
@@ -21,10 +20,11 @@ img_0.save("img_0.bmp")
 # 创建一个白色图像
 img_1 = Image.new('RGB', (width, height), color='white')
 
-for i in range(0,16):
-    for y in range(i*16,i*16+16):
-        for x in range(img_1.width):
-            img_1.putpixel((x, y), (255-i*16, 255-i*16, 255-i*16))
+for u in range(0,16):
+    for i in range(u*16,u*16+16):
+        for y in range(i,i+1):
+            for x in range(img_1.width):
+                img_1.putpixel((x, y), (255-(i-u*16)*16, 255-(i-u*16)*16, 255-(i-u*16)*16))
 
 # 保存图像
 img_1.save("img_1.bmp")
